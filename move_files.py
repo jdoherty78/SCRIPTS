66873593a313d74d87a9ca355254a2be7096e247
jdoherty78 <joedoherty78@gmail.com>
2024-01-03 09:48:19 -0500

    Adding move_files.py to repo

---
+++ move_files.py
@@ -0,0 +1,19 @@
+import shutil, os
+
+NEW = input("Enter a new folder: ")
+OLD = input("Enter a folder that exists: ")
+
+if OLD not in os.listdir("."):
+    print("This folder does not exist. Try again")
+else:
+    all_files = [i for i in os.listdir(OLD)
+	         if "." in i and i.split(".")[1] in ["ipynb", "txt"]]
+
+    if not os.path.exists(NEW):
+        os.mkdir(NEW)    
+
+    for f in all_files:
+        old = OLD +"/" + f
+        new = NEW + "/" + f
+        shutil.move(old, new)
+    print("Files have been moved to {}".format(NEW))
