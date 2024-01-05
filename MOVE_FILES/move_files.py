9cf4e78867437093721f9409910e0c38febdf5c8
Joe <47543330+jdoherty78@users.noreply.github.com>
2024-01-05 15:46:22 -0500

    Update move_files.py
    --adding commentary

--- MOVE_FILES/move_files.py
+++ MOVE_FILES/move_files.py
@@ -7,7 +7,7 @@ if OLD not in os.listdir("."):
     print("This folder does not exist. Try again")
 else:
     all_files = [i for i in os.listdir(OLD)
-	         if "." in i and i.split(".")[1] in ["ipynb", "txt"]]
+	         if "." in i and i.split(".")[1] in ["ipynb", "txt"]]  #file extensions to focus on
 
     if not os.path.exists(NEW):
         os.mkdir(NEW)    
